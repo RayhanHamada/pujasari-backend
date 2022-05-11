2abd35f879ff4b9ca4e135da96d15272a4a8396b
RayhanHamada <rayhanhamada1999@gmail.com>
2022-05-12 01:06:26 +0700

    feat: install stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import { fastify } from 'fastify';
+
+const app = fastify({ logger: true });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (_err, addr) => {
+  console.log(`running on ${addr}`);
+});
