3a1f5f67e03bdcbff626e244e042869640c87d62
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-06-18 22:17:36 +0700

    chore: install tsx and config more

--- src/common/firebase.ts
+++ src/common/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import constants from 'src/constants';
 
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG!);
+const firebaseConfig = JSON.parse(constants.FIREBASE_CONFIG!);
 
 const firebase = initializeApp(firebaseConfig);
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 const constants = {
   IS_PROD: process.env.NODE_ENV === 'production',
   PORT: process.env.PORT || 4000,
+  FIREBASE_CONFIG: process.env.FIREBASE_CONFIG,
 };
 
 export default constants;
@@ -9,5 +10,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV?: string;
     PORT?: string;
+    FIREBASE_CONFIG?: string;
   }
 }
